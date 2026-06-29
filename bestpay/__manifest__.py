dc8d85bee04254ab91e62aa2519ca04356008618
Samueljg15 <samueljgm15@gmail.com>
2026-06-29 16:34:20 -0400

    feat: creación del módulo bestpay

---
+++ bestpay/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'BestPay',
+    'version': '1.0.0',
+    'category': 'Accounting',
+    'summary': 'Módulo de integración de pagos BestPay',
+    'description': 'Verificador de pagos y enlaces de pago para Steam Solutions',
+    'author': 'Samuel',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
