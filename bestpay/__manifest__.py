b0369ac7fd6cb097046340df939e74a77be78398
Wilson <wilram334@yandex.com>
2026-06-30 17:55:46 -0400

    Bestpay en payment transaction

--- bestpay/__manifest__.py
+++ bestpay/__manifest__.py
@@ -1,12 +1,20 @@
 {
     'name': 'BestPay',
-    'version': '1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Módulo de integración de pagos BestPay',
     'description': 'Verificador de pagos y enlaces de pago para Steam Solutions',
     'author': 'Samuel',
-    'depends': ['base'],
-    'data': [],
+    'depends': [
+        'base',
+        'payment',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'views/payment_transaction_views.xml',
+        'views/bestpay_menus.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
